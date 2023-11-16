a7040d0d87d64ba8b2f956e250df58ca40395832
boblyx <bobyxlee@gmail.com>
2023-11-16 22:31:17 +0800

    Implemented very basic echo, but has limits

--- src/reply_input/reply.s
+++ src/reply_input/reply.s
@@ -8,38 +8,35 @@ _start:
     .equ stdin, 0
     .equ stdout, 1
     li  a0, stdout
-l1:
+L1:
     li  a0, 1 # file descriptor id
     la  a1, msg # buffer
     li  a2, 13 # size_t of buffer
     li  a7, 64 # sys_write
     ecall
     
-l2:
-    lui a1, %hi(buff)
-    addi a1, a1, %lo(l2)
+L2:
+    lui a1, %hi(buff) # Load buffer into a1
     li  a0, stdin # file descriptor id
     li  a2, 5 # size_t of buffer
     li  a7, 63 # sys_read
     ecall
 
-l3:
-    lui a1, %hi(buff)
-    addi a1, a1, %lo(l3)
-    li a2, 5
-    li a3, 0
-    li a7, 64
+L3:
+    lui a1, %hi(buff) # Load buffer into a1
+    li a0, 1 # file descriptor id
+    li a2, 5 # size_t of buffer
+    li a7, 64 # sys_write
     ecall
     
-    # TODO: Print read message.
-    
+epilogue: 
     li  a7, 93 # sys_exit
-    li  a0, 13
+    li  a0, 0 # exit_code
     ecall
 
 .data
 msg:
     .string "Enter string\n"
 buff:
-    .zero 32
+    .zero   32 # size_t of expected buffer
 
